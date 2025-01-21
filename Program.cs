716c1f8395e760515bd89abff977140d70f6a6c5
aishanoor1439 <aisha.noor1439@gmail.com>
2025-01-21 09:46:20 +0100

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Root_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Square Root Calculator!");
+
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                double number = Convert.ToDouble(input);
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Error: Cannot compute square root of a negative number.");
+                }
+                else
+                {
+                    double result = Math.Sqrt(number);
+                    Console.WriteLine("The square root of {0} is {1}", number, result);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
